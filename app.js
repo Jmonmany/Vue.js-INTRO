bc295853e904b2711254366b6e0cfd2ceab7361b
Javi <monmany1992@gmail.com>
2023-04-08 16:33:38 +0200

    Add v-for directive

--- app.js
+++ app.js
@@ -49,3 +49,44 @@ const app = Vue.createApp({
 });
 // Mount the app to the DOM
 app.mount('#myApp')
+
+const quotes = [
+  {
+    quote:
+      "The night is darkest just before the dawn. And I promise you, the dawn is coming.",
+    author: "Harvey Dent, The Dark Knight",
+  },
+  {
+    quote: "I believe what doesn’t kill you simply makes you, stranger.",
+    author: "The Joker, The Dark Knight",
+  },
+  {
+    quote:
+      "Your anger gives you great power. But if you let it, it will destroy you… As it almost did me",
+    author: "Henri Ducard, Batman Begins",
+  },
+  {
+    quote:
+      "You either die a hero or live long enough to see yourself become the villain.",
+    author: "Harvey Dent, The Dark Knight",
+  },
+  {
+    quote: "If you’re good at something, never do it for free.",
+    author: "The Joker, The Dark Knight",
+  },
+  {
+    quote: "Yes, father. I shall become a bat.",
+    author: "Bruce Wayne/Batman, Batman: Year One",
+  },
+];
+
+const app2 = Vue.createApp({
+  data() {
+    return {
+      message: "Batman quotes",
+      quotes, // this is the same as quotes: quotes, and works because,
+      // the key and the value have the same name
+    };
+  },
+});
+app2.mount('#myApp2')
